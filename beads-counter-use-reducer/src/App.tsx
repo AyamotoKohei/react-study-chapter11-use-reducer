f260cdd3abb1240b2f0614db5a6f6889c362ad37
AyamotoKohei <koh14taka2@gmail.com>
2021-11-22 23:12:00 +0900

    add: React Appの追加

---
+++ beads-counter-use-reducer/src/App.tsx
@@ -0,0 +1,18 @@
+import { VFC } from 'react';
+
+import CounterBoard from 'containers/organisms/CounterBoard';
+import ColorfulBeads from 'containers/molecules/ColorfulBeads';
+
+import './App.css';
+
+const App: VFC = () => (
+  <div className="container">
+    <header>
+      <h1>ビーズカウンター</h1>
+    </header>
+    <CounterBoard />
+    <ColorfulBeads />
+  </div>
+);
+
+export default App;
